59ba1362ca6d50a291111d5d6a113f21f711cf9e
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-11-30 20:35:42 -0800

    added workbox plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
@@ -21,6 +22,21 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "JATE",
+      }),
+      new WorkboxPlugin.GenerateSW({
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 2,
+              },
+            },
+          },
+        ],
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
@@ -50,16 +66,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
